816ea18881832bbe57601959873ed76b9513d971
Nathalye Jessica <rafaelamamaeteama@gmail.com>
2024-09-26 12:04:03 -0300

    desenvolvendo interface grafica

---
+++ funcoes.py
@@ -0,0 +1,9 @@
+def soma(n1, n2):
+    return n1 + n2
+
+def somaLista(lista):
+    rs = 0 
+    for i in lista:
+        rs+=i
+        print(f"O resultado da soma é {rs}")
+        
---
+++ usar_funcoes.py
@@ -0,0 +1,10 @@
+
+# import funcoes as fn
+
+
+from funcoes import soma as sm
+
+s = fn.soma(10,2)
+print(f"O resultado é {s}")
+
+
